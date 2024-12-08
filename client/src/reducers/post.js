8695f81bfd8e86f109d88a33066ed70e01963bf5
GaytriJindam <gayatrijindam121@gmail.com>
2024-12-08 16:29:20 +0000

    CommentItem function

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,4 +1,4 @@
-import { ADD_POST, DELETE_POST, GET_POSTS,POST_ERROR, UPDATE_LIKES } from "../actions/types"
+import { ADD_COMMENT, ADD_POST, DELETE_POST, GET_POST, GET_POSTS,POST_ERROR, REMOVE_COMMENT, UPDATE_LIKES } from "../actions/types"
 
 const initialState={
     posts:[],
@@ -12,6 +12,12 @@ export default function(state = initialState, action)
     const { type, payload} = action;
 
     switch(type){
+        case GET_POST:
+            return {
+                ...state,
+                post: payload,
+                loading:false  
+            };
         case GET_POSTS:
             return {
                 ...state,
@@ -43,6 +49,23 @@ export default function(state = initialState, action)
                         { ...post, likes: payload.likes } : post),
                         loading: false
                         };
+
+                case ADD_COMMENT:
+                    return {
+                        ...state,
+                        post: { ...state.post, comments: payload},
+                        loading: false
+                    };
+
+                case REMOVE_COMMENT:
+                    return {
+                        ...state,
+                        post: {
+                            ...state.post,
+                            comments: state.post.comments.filter(
+                            comment => comment._id !== payload)},
+                        loading: false
+                    };
                 default:
                     return state;
     
